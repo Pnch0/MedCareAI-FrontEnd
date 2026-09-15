035bc14b7921a46dfbd9d32e6bf09f9395672e4e
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-15 11:10:10 -0300

    feat: modificacion del archivo App.tsx para un correcto funcionamiento de la pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { Toaster } from 'sonner';
+import { ProtectedRoute, PublicRoute } from './Components/Routes/ProtectedRoutes.tsx';
+import MainLayout from './Components/Layouts/MainLayout/MainLayout.tsx';
 
 function App() {
 
   return (
     <>
-      
+      <Toaster 
+        position="top-right" 
+        richColors 
+        theme="dark" 
+        closeButton 
+      />
+
+      <Router>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<LoginPage />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route element={<MainLayout />}>
+
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
