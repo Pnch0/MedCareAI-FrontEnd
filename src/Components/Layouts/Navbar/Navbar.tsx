d397ffb1047e1aac86a29055b9d1aad28e70096c
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-23 18:51:38 -0300

    feat: incorporacion de navbar junto con su respectivo diseño, archivo Navbar.tsx - Navbar.css

--- src/Components/Layouts/Navbar/Navbar.tsx
+++ src/Components/Layouts/Navbar/Navbar.tsx
@@ -1,11 +1,47 @@
 import './Navbar.css';
+import { FaHome, FaCalendar, FaBook  } from "react-icons/fa";
+import { FaClipboardList } from "react-icons/fa6";
+import { NavLink, useNavigate, useSearchParams } from 'react-router-dom';
+
 
 function Navbar(){
+    const navigate = useNavigate();
 
     return(
         <>
             <div className="Contenedor-NavbarPrincipal">
-                
+                <div className="Contenedor-PaginasNavbar">
+                    <ul>
+                        <li>
+                            <NavLink to="/" className = "nav-item">
+                                <FaHome className='Icono-NavbarPrincipal'/> Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" className = "nav-item">
+                                <FaCalendar  className='Icono-NavbarPrincipal'/> Agenda
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" className = "nav-item">
+                                <FaClipboardList  className='Icono-NavbarPrincipal'/> Resumen
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/" className = "nav-item">
+                                <FaBook  className='Icono-NavbarPrincipal'/> Gestion Citas
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="Contenedor-NombreNavbar">
+                    <div className="Contenedor-InicialesNombre">
+                        WF
+                    </div>
+                    <div className="Contenedor-Nombre">
+                        Wilson Flores
+                    </div>
+                </div>
             </div>
         </>
     )
