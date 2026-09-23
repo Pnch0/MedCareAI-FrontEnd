c45d905c3ceab405b3a3d0b868fc468b9b50d92a
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-23 20:40:44 -0300

    feat: creacion de Homepage del medico ademas de su diseño, archivo HomePaige.tsx - HomePage.css

--- src/Pages/Medicos/HomePage/HomePage.tsx
+++ src/Pages/Medicos/HomePage/HomePage.tsx
@@ -1,11 +1,50 @@
-
+import "./HomePage.css"
 
 function HomePageMedico(){
 
     return(
 
         <>
-        <h1>a</h1>
+        <div className="ContenedorPrincipal-HomeMedico">
+            <div className="ContenedorNombre-HomeMedico">
+                <h1>Bienvenido Wilson Flores</h1>
+            </div>
+            <div className="Contenedor-DashboardMedico">
+                <div className="ContenedorDarboard-Izquierda">
+                    <div className="Contenedor-AgendaMedico">
+                        <div className="Contenedor-EncabezadoAgenda">
+                            <h2>Agenda</h2>
+                        </div>
+                        <div className="Contenedor-ListadoAgenda">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="ContenedorDarboard-Derecha">
+                    <div className="Contenedor-ResumenClinico">
+                        <div className="Titulo-ResumenClinico">
+                            <h3>Resumen Clinico</h3>
+                        </div>
+                        <div className="ContenedorListado-ResumenClinico">
+                            <h3>Juan Perez</h3>
+                            <p>Edad: 50 años</p>
+                            <div className="ContenedorDiagnostico-ResumenClinico">
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="Contenedor-ControlCita">
+                        <div className="Titulo-ControlCita">
+                            <h3>Control Gestion de Cita</h3>
+                        </div>
+                        <div className="ContenedorListado-ControlCita">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
 
     )
